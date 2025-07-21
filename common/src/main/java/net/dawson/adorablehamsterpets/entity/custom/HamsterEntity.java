f3490e43fd5b8c0c6b22665777886a2ec4cd0cbb
The Scarlet Fox <tweekythefox@gmail.com>
2025-07-21 08:17:02 -0500

    [STYLE] Adjust hamster flee speed to match wander speed

--- common/src/main/java/net/dawson/adorablehamsterpets/entity/custom/HamsterEntity.java
+++ common/src/main/java/net/dawson/adorablehamsterpets/entity/custom/HamsterEntity.java
@@ -2615,7 +2615,7 @@ public class HamsterEntity extends TameableEntity implements GeoEntity, Implemen
         this.goalSelector.add(2, new HamsterMeleeAttackGoal(this, 1.0D, true));
         this.goalSelector.add(3, new HamsterMateGoal(this, 1.0D));
         this.goalSelector.add(4, new HamsterFollowOwnerGoal(this, 1.2D, 4.0F, 16.0F));
-        this.goalSelector.add(5, new HamsterFleeGoal<>(this, LivingEntity.class, 8.0F, 1.0D, 1.5D));
+        this.goalSelector.add(5, new HamsterFleeGoal<>(this, LivingEntity.class, 8.0F, 0.75D, 1.5D));
         this.goalSelector.add(6, new HamsterTemptGoal(this, 1.2D,
                 Ingredient.ofItems(ModItems.SLICED_CUCUMBER.get(), ModItems.CHEESE.get(), ModItems.STEAMED_GREEN_BEANS.get()),
                 false));
