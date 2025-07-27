2c2cc4da07ef7d0a5305856af583fb91405b78f9
The Scarlet Fox <tweekythefox@gmail.com>
2025-07-27 09:32:03 -0500

    [FIX] Add forgotten config-driven biome feature spawning in ConfigurableFeatureModifier.java

--- neoforge/src/main/java/net/dawson/adorablehamsterpets/world/neoforge/ConfigurableFeatureModifier.java
+++ neoforge/src/main/java/net/dawson/adorablehamsterpets/world/neoforge/ConfigurableFeatureModifier.java
@@ -35,7 +35,7 @@ public record ConfigurableFeatureModifier(
             this.featuresToAdd.ifPresent(additions -> {
                 for (RegistryEntry<PlacedFeature> feature : additions) {
                     // Use centralized helper methods to decide if the feature should be added to this specific biome
-                    if (shouldFeatureBeInBiome(feature, biome)) {
+                    if (ModWorldGeneration.shouldFeatureSpawnInBiome(feature, biome)) {
                         builder.getGenerationSettings().getFeatures(GenerationStep.Feature.VEGETAL_DECORATION).add(feature);
                     }
                 }
@@ -44,23 +44,12 @@ public record ConfigurableFeatureModifier(
         if (phase == Phase.REMOVE && this.biomes.contains(biome)) {
             this.featuresToRemove.ifPresent(removals -> {
                 for (RegistryEntry<PlacedFeature> feature : removals) {
-                    if (shouldFeatureBeInBiome(feature, biome)) {
+                    if (ModWorldGeneration.shouldFeatureSpawnInBiome(feature, biome)) {
                         builder.getGenerationSettings().getFeatures(GenerationStep.Feature.VEGETAL_DECORATION).remove(feature);
                     }
                 }
             });
         }
-    }
-
-    private boolean shouldFeatureBeInBiome(RegistryEntry<PlacedFeature> feature, RegistryEntry<Biome> biome) {
-        // This is where we check against our centralized logic
-        String featurePath = feature.getKey().map(key -> key.getValue().getPath()).orElse("");
-        return switch (featurePath) {
-            case "custom_sunflower_placed", "patch_sunflower" -> ModWorldGeneration.isSunflowerPlains(biome);
-            case "wild_green_bean_bush_placed" -> ModWorldGeneration.isGreenBeanBiome(biome);
-            case "wild_cucumber_bush_placed" -> ModWorldGeneration.isCucumberBiome(biome);
-            default -> true; // Default to true if no specific logic, allows for future simple additions
-        };
     }
 
     @Override
