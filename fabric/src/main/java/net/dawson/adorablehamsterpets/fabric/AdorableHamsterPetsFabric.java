8e4f6bd187ce1102b197bf21b7df6d935ccafa87
The Scarlet Fox <tweekythefox@gmail.com>
2025-07-27 07:12:10 -0500

    [FIX] Correct spawning initialization logic in AdorableHamsterPetsFabric.java

--- fabric/src/main/java/net/dawson/adorablehamsterpets/fabric/AdorableHamsterPetsFabric.java
+++ fabric/src/main/java/net/dawson/adorablehamsterpets/fabric/AdorableHamsterPetsFabric.java
@@ -1,8 +1,13 @@
 package net.dawson.adorablehamsterpets.fabric;
 
+import dev.architectury.registry.level.biome.BiomeModifications;
 import net.dawson.adorablehamsterpets.AdorableHamsterPets;
+import net.dawson.adorablehamsterpets.config.Configs;
+import net.dawson.adorablehamsterpets.entity.ModEntities;
 import net.dawson.adorablehamsterpets.world.gen.ModEntitySpawns;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.world.biome.SpawnSettings;
 
 public final class AdorableHamsterPetsFabric implements ModInitializer {
     @Override
@@ -10,6 +15,29 @@ public final class AdorableHamsterPetsFabric implements ModInitializer {
         AdorableHamsterPets.initRegistries();
         AdorableHamsterPets.initAttributes();
         AdorableHamsterPets.initCommonSetup();
-        ModEntitySpawns.initialize();
+        BiomeModifications.addProperties(
+                ModEntitySpawns::shouldSpawnInBiome, // Use the common decider method
+                (context, props) -> {
+                    props.getSpawnProperties().addSpawn(
+                            SpawnGroup.CREATURE,
+                            new SpawnSettings.SpawnEntry(
+                                    ModEntities.HAMSTER.get(),
+                                    Configs.AHP.spawnWeight.get(),
+                                    1,
+                                    Configs.AHP.maxGroupSize.get()
+                            )
+                    );
+                }
+        );
     }
 }
+
+
+
+
+
+
+
+
+
+
