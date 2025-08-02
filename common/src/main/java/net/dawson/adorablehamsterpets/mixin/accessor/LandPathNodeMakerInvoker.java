9606fb43e39ab31c501e6ed9003070f42df21940
The Scarlet Fox <tweekythefox@gmail.com>
2025-08-02 10:20:15 -0500

    [CHORE] Remove unused imports from LandPathNodeMakerInvoker.java

--- common/src/main/java/net/dawson/adorablehamsterpets/mixin/accessor/LandPathNodeMakerInvoker.java
+++ common/src/main/java/net/dawson/adorablehamsterpets/mixin/accessor/LandPathNodeMakerInvoker.java
@@ -2,11 +2,8 @@ package net.dawson.adorablehamsterpets.mixin.accessor;
 
 import net.minecraft.entity.ai.pathing.LandPathNodeMaker;
 import net.minecraft.entity.ai.pathing.PathNodeType;
-import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.BlockView;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
