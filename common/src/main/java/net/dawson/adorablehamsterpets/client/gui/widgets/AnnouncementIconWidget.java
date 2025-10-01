a545f111f8e3afa63d34ca63588d1a8f03bdc5db
The Scarlet Fox <tweekythefox@gmail.com>
2025-10-01 08:11:34 -0500

    [FIX] Refactor nested ConfigGroups to ValidatedAny to work around FzzyConfig GUI bug

--- common/src/main/java/net/dawson/adorablehamsterpets/client/gui/widgets/AnnouncementIconWidget.java
+++ common/src/main/java/net/dawson/adorablehamsterpets/client/gui/widgets/AnnouncementIconWidget.java
@@ -68,12 +68,12 @@ public class AnnouncementIconWidget extends ButtonWidget {
             // offsets for creative and survival mode to accommodate their unique shapes.
             if (containerScreen instanceof net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen) {
                 // Creative Inventory (outside bottom right corner, to avoid conflicting with extra inventory tabs)
-                targetX = guiLeft + guiWidth + 5 + Configs.AHP.creativeWidgetIconOffsetX.get();
-                targetY = guiTop + 139 + Configs.AHP.creativeWidgetIconOffsetY.get();
+                targetX = guiLeft + guiWidth + 5 + Configs.AHP.creativeWidgetIconSettings.get().offsetX.get();
+                targetY = guiTop + 139 + Configs.AHP.creativeWidgetIconSettings.get().offsetY.get();
             } else {
                 // Survival Inventory (overlapping top right corner)
-                targetX = guiLeft + guiWidth - this.width + 4 + Configs.AHP.survivalWidgetIconOffsetX.get();
-                targetY = guiTop - 4 + Configs.AHP.survivalWidgetIconOffsetY.get();
+                targetX = guiLeft + guiWidth - this.width + 4 + Configs.AHP.survivalWidgetIconSettings.get().offsetX.get();
+                targetY = guiTop - 4 + Configs.AHP.survivalWidgetIconSettings.get().offsetY.get();
             }
 
             // If the target position has changed (e.g., recipe book opened), start a new transition.
